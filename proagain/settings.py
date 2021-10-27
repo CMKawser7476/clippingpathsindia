f7dec5739b1ac2b62a506284cbd49346c43cd70e
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2021-10-28 01:10:18 +0600

    added * on allowed host & added req.txt

--- proagain/settings.py
+++ proagain/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#*oot1n6=v1(uudsue5)=w!k=^--#3%n#jo8y@-l!eq176#bo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
